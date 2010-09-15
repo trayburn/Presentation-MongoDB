f67b2f6b2b527a1214faaa3b09fe5bab72ee2618
Tim Rayburn <tim@timrayburn.net>
2010-09-15 16:07:41 -0700

    Pre Populator

--- Presentation-MongoDB/Populator/Program.cs
+++ Presentation-MongoDB/Populator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MongoDB;
 
 namespace Populator
 {
@@ -9,6 +10,28 @@ namespace Populator
     {
         static void Main(string[] args)
         {
+            var firstNames = new[] { "Tim", "Dave", "Craig", "Noah", "Mark", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            var lastNames = new[] { "Rayburn", "O'Hara", "Neuwirt", "Coad", "Mikaelis" };
+
+            using (var mongo = new Mongo())
+            {
+                mongo.Connect();
+                var db = mongo.GetDatabase("StElmo");
+                var coll = db.GetCollection("Patients");
+                foreach (string firstName in firstNames)
+                {
+                    foreach (string lastName in lastNames)
+                    {
+                        coll.Save(new Document()
+                            .Add("FirstName", firstName)
+                            .Add("LastName", lastName));
+                        Console.Write(".");
+                    }
+                }
+            }
+
+            Console.WriteLine("Complete");
+            Console.ReadLine();
         }
     }
 }
