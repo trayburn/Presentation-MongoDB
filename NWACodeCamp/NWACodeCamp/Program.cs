33cd37e14a90d66ee1f211aed64e10626201e4d7
Tim Rayburn <tim@timrayburn.net>
2011-01-26 20:56:51 -0600

    Post NWA Code Camp

---
+++ NWACodeCamp/NWACodeCamp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.Components.DictionaryAdapter;
+
+namespace NWACodeCamp
+{
+    class Program
+    {
+        // github.com/samus/mongodb-csharp
+        // github.com/atheken/norm
+
+        // github.com/mongo/mongo-csharp-driver
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dict = new Dictionary<string, object>()
+                {
+                    {"FirstName","Tim"}, {"LastName","Rayburn"}, {"MiddleName","John"}
+                };
+
+                var da = new DictionaryAdapterFactory().GetAdapter<IPerson>(dict);
+
+                Console.WriteLine(da.FirstName);
+                Console.WriteLine(da.LastName);
+
+                da.FirstName = "Chris";
+                da.Aliases = new List<string>() { "Bob", "Frank", "Henry" };
+
+                Console.WriteLine(dict["FirstName"]);
+                Console.WriteLine(dict["Aliases"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine("Complete");
+            Console.ReadLine();
+        }
+    }
+
+    public interface IPerson
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        [StringList(Separator='|')]
+        List<string> Aliases { get; set; }
+    }
+}
--- Presentation-MongoDB/Populator/Program.cs
+++ Presentation-MongoDB/Populator/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MongoDB;
+using Castle.Components.DictionaryAdapter;
+using MongoDB.Linq;
 
 namespace Populator
 {
@@ -13,25 +15,47 @@ namespace Populator
             var firstNames = new[] { "Tim", "Dave", "Craig", "Noah", "Mark", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
             var lastNames = new[] { "Rayburn", "O'Hara", "Neuwirt", "Coad", "Mikaelis" };
 
+            var daf = new DictionaryAdapterFactory();
+            var da = daf.GetAdapter<IPatient>(new Document()
+                .Add("FirstName", "Tim")
+                .Add("LastName", "Rayburn")
+                .Add("Address", new Document().Add("Line1","Dallas,TX")));
+
             using (var mongo = new Mongo())
             {
-                mongo.Connect();
                 var db = mongo.GetDatabase("StElmo");
-                var coll = db.GetCollection("Patients");
-                foreach (string firstName in firstNames)
-                {
-                    foreach (string lastName in lastNames)
-                    {
-                        coll.Save(new Document()
-                            .Add("FirstName", firstName)
-                            .Add("LastName", lastName));
-                        Console.Write(".");
-                    }
-                }
+                var coll = db.GetCollection<Patient>().Linq().Select(p => p.FirstName == "Tim").ToList();
+                var query = from p in db.GetCollection<Patient>().Linq()
+                            where p.FirstName == "Tim"
+                            select p;
             }
+
+            Console.WriteLine(da.FirstName);
+            Console.WriteLine(da.Address.Line1);
 
             Console.WriteLine("Complete");
             Console.ReadLine();
         }
+    }
+
+    public interface IPatient
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+        [Component]
+        IAddress Address { get; set; }
+    }
+
+    public class Patient
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    } 
+    public interface IAddress
+    {
+        string Line1 { get; set; }
     }
 }
